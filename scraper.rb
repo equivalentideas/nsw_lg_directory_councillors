a9f9ee8ea4ec6c5aa22cbf8ea6b22d5148933190
Matthew Landauer <matthew@oaf.org.au>
2015-03-17 16:58:14 +1100

    Parse councillors from a single council

--- scraper.rb
+++ scraper.rb
@@ -13,4 +13,35 @@ else
   end
 end
 
-puts data
+def parse_council(text)
+  p text
+  record = {councillors: []}
+  record[:name] = text.lines[0].strip
+  if text.lines[10] =~ /Councillors \((\d+)\)/
+    record[:no_councillors] = $1.to_i
+  else
+    raise "Unexpected format for number of councillors line"
+  end
+  if text.lines[11].split("\t")[0] == "Mayor"
+    record[:councillors] << {name: text.lines[11].split("\t")[1].strip, position: "mayor"}
+  else
+    raise "Unexpected format for mayor line"
+  end
+  if text.lines[12].split("\t")[0] == "Deputy"
+    record[:councillors] << {name: text.lines[12].split("\t")[1].strip, position: "deputy mayor"}
+  else
+    raise "Unexpected format for deputy mayor line"
+  end
+  text.lines[13].split(",").each do |t|
+    record[:councillors] << {name: t.strip, position: "councillor"}
+  end
+  # Do a sanity check
+  raise "Councillor numbers not consistent" unless record[:councillors].count == record[:no_councillors]
+  record.delete(:no_councillors)
+  record
+end
+
+# Skip first 6 lines
+# And split into each council on double blank line
+
+p parse_council(data.lines[6..-1].join.split("\r\n\r\n\r\n")[0])
